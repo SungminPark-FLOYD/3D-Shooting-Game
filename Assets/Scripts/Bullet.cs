0c38afa31138a7b03a9b3bfeedc711eb8f81ab66
FLOYD-psm <107343662+SungminPark-FLOYD@users.noreply.github.com>
2022-11-29 16:58:40 +0900

    Enemy Nav
    Enemy Nav

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public LayerMask collisionMask;
     float speed = 10;
     
     public void SetSpeed(float newSpeed)
@@ -12,6 +13,24 @@ public class Bullet : MonoBehaviour
     }
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        GameObject.Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathfinder;
+    Transform target;
+    void Start()
+    {
+        //플레이어 태그를 찾아 올수있게 설정
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator UpdatePath()
+    {
+        //갱신시간
+        float refreshRate = 0.25f;
+
+        while(target != null)
+        {
+            //타겟 위치 가져오기
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate); //1초마다 루프 반복
+        }
+    }
+}
