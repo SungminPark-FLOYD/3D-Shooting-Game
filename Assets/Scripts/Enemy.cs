7a2005ad5ed5350d39aa8413f390e8971ee4416d
FLOYD-psm <107343662+SungminPark-FLOYD@users.noreply.github.com>
2022-12-30 16:38:11 +0900

    Enemy Attack
    Implementation of enemy attacks using state patterns

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,14 +7,35 @@ using UnityEngine.UIElements;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State { Idle, Chasing, Attacking};
+    State currentState;
+
     NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
+
+    Color originalColor;
+
+    float attackDistanceThreshold = 0.5f; //공격거리
+    float timeBetweenAttacks = 1;
+
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
+
     protected override void Start()
     {
         base.Start();
         //플레이어 태그를 찾아 올수있게 설정
         pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColor = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
 
         StartCoroutine(UpdatePath());
     }
@@ -22,7 +43,46 @@ public class Enemy : LivingEntity
     
     void Update()
     {
+        if (Time.time > nextAttackTime)
+        {
+            float sqrDisToTarget = (target.position - transform.position).sqrMagnitude;
+
+            if (sqrDisToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+            {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 dirToTarget = (target.position - transform.position).normalized;
+        //타겟 위치 가져오기
+        Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius + attackDistanceThreshold / 2);
         
+
+        float attackSpeed = 3f;
+        float percent = 0;
+
+        skinMaterial.color = Color.red;
+
+        while(percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+
+            //float interpolation = (-percent * percent + percent) * 4;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+            yield return null;
+        }
+        skinMaterial.color = originalColor;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath()
@@ -32,11 +92,15 @@ public class Enemy : LivingEntity
 
         while(target != null)
         {
-            //타겟 위치 가져오기
-            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-            if (!dead)
+            if (currentState == State.Chasing)
             {
-                pathfinder.SetDestination(targetPosition);
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                //타겟 위치 가져오기
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold/2);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+                }
             }
             yield return new WaitForSeconds(refreshRate); //1초마다 루프 반복
         }
